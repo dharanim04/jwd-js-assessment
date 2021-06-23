2b213e1b4e39c3bdf43f7c03e8357af0d91b01e8
dharanim04 <dharani.m04@gmail.com>
2021-06-23 14:07:07 +1000

    completed 5 tasks

--- js/index.js
+++ js/index.js
@@ -19,44 +19,55 @@
       5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
-window.addEventListener('DOMContentLoaded', () => {
-  const start = document.querySelector('#start');
-  start.addEventListener('click', function (e) {
-    document.querySelector('#quizBlock').style.display = 'block';
-    start.style.display = 'none';
+window.addEventListener("DOMContentLoaded", () => {
+  const start = document.querySelector("#start");
+  start.addEventListener("click", function (e) {
+    document.querySelector("#quizBlock").style.display = "block";
+    start.style.display = "none";
+    timeCounter();
   });
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
   const quizArray = [
     {
-      q: 'Which is the third planet from the sun?',
-      o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
+      q: "Which is the third planet from the sun?",
+      o: ["Saturn", "Earth", "Pluto", "Mars"],
       a: 1, // array index 1 - so Earth is the correct answer here
     },
     {
-      q: 'Which is the largest ocean on Earth?',
-      o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+      q: "Which is the largest ocean on Earth?",
+      o: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
       a: 3,
     },
     {
-      q: 'What is the capital of Australia',
-      o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+      q: "What is the capital of Australia?",
+      o: ["Sydney", "Canberra", "Melbourne", "Perth"],
       a: 1,
     },
+    // {
+    //   q: 'What is the capital of India?',
+    //   o: ['Delhi', 'Hyderabad', 'Chennai', 'Mumbai'],
+    //   a: 0,
+    // },
+    // {
+    //   q: 'When did our JWD cohort-5 started',
+    //   o: ['December', 'May', 'April', 'August'],
+    //   a: 2,
+    // },
   ];
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
-    const quizWrap = document.querySelector('#quizWrap');
-    let quizDisplay = '';
+    const quizWrap = document.querySelector("#quizWrap");
+    let quizDisplay = "";
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
                    Q - ${quizItem.q}
-                    <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
-                    <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
-                    <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
-                    <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
+                    <li class="list-group-item list-group-item-action mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> <label for="radio_${index}_0">  ${quizItem.o[0]} </label></li>
+                    <li class="list-group-item list-group-item-action " id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"><label for="radio_${index}_1"> ${quizItem.o[1]} </label></li>
+                    <li class="list-group-item list-group-item-action"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"><label for="radio_${index}_2"> ${quizItem.o[2]} </label></li>
+                    <li class="list-group-item list-group-item-action"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"><label for="radio_${index}_3"> ${quizItem.o[3]} </label></li>
                     </ul>
                     <div>&nbsp;</div>`;
       quizWrap.innerHTML = quizDisplay;
@@ -71,20 +82,88 @@ window.addEventListener('DOMContentLoaded', () => {
         //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
         let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
-
+        liElement = document.querySelector("#" + li);
+        radioElement = document.querySelector("#" + r);
+        //disabling the list items after submit
+        liElement.className="list-group-item disabled";
         if (quizItem.a == i) {
           //change background color of li element here
+          document.getElementById(li).style.backgroundColor = " rgba(152, 243, 174, 0.4)";
         }
 
-        if (radioElement.checked) {
+        if (radioElement.checked && quizItem.a == i) {
           // code for task 1 goes here
+          score++;
         }
       }
     });
+    document.getElementById("score").innerHTML = `Your Score is: ${score}`;
+    document.getElementById("time").innerHTML = "0";
   };
 
   // call the displayQuiz function
   displayQuiz();
+  //declare submit button
+  const submit = document.getElementById("btnSubmit");
+  //add event to calcuate score on click
+  submit.addEventListener("click", calculateScore);
+
+  const reset = document.getElementById("btnReset");
+
+  reset.addEventListener("click", function (e) {
+    // console.log("windolocat:"+window.location.href);
+    window.location.assign(window.location.href);
+//another way of reset button 
+
+    // document.querySelector("#quizBlock").style.display = "none";
+    // start.style.display = "block";
+    // document.getElementById("score").innerHTML = "";
+    // quizArray.map((quizItem, index) => {
+    //   for (let i = 0; i < 4; i++) {
+    //     //highlight the li if it is the correct answer
+    //     let li = `li_${index}_${i}`;
+    //     liElement = document.querySelector("#" + li);
+    //     if (quizItem.a == i) {
+    //       //change background color of li element here
+    //       document.getElementById(li).style.backgroundColor = "";
+    //     }
+    //   }
+    //   document.getElementById("time").innerHTML = "30sec";
+
+    // });
+  });
+  //end form
 });
+const timeCounter = () => {
+  var countDownDate1 = new Date();
+  countDownDate1.setSeconds(countDownDate1.getSeconds() + 15);
+  var countDownDate = countDownDate1.getTime();
+
+  
+
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    let time = document.getElementById("time").innerHTML;
+    // Get today's date and time
+    var now = new Date().getTime();
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    // Time calculations for seconds
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Output the result in an element with id="time"
+    if(time == "0"){
+      clearInterval(x);
+    } else{
+    document.getElementById("time").innerHTML = seconds + "s ";
+    }
+    // If the count down is over, write some text
+    if (distance < 0 ) {
+        clearInterval(x);
+        $("#btnSubmit").trigger("click");
+        document.getElementById("time").innerHTML = "EXPIRED";
+      
+     
+    }
+  }, 1000);
+};
